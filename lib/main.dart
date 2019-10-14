7b6784f9fd39fc2eeb31cff6ff55a67bc6bf8b5c
mehmetyildiz7 <test@temp.com>
2019-10-14 14:33:18 +0300

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avatar.png'),
+                backgroundColor: Colors.pinkAccent,
+              ),
+              Text(
+                'İpek Nil Şahin',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.black,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
